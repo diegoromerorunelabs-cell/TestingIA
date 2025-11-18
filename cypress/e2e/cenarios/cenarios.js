3ad45bcb418cee3012bba48fc5cbd3dcc9579928
diegoromerorunelabs-cell <diegoromerorunelabs@gmail.com>
2025-11-17 20:04:30 -0600

    Add step definitions for KAN-16, KAN-13 and KAN-15

--- cypress/e2e/cenarios/cenarios.js
+++ cypress/e2e/cenarios/cenarios.js
@@ -35,3 +35,64 @@ Then("Blogs page is loaded", () => {
   cy.wait(2000); // Esperar a que la página cargue completamente
   cy.contains('HTML Basics').should('be.visible');
 });
+
+// --- New step definitions for KAN-16, KAN-13, KAN-15 ---
+
+Then("\"Start Exploring\" button is visible", () => {
+  cy.contains('Start Exploring').should('be.visible');
+});
+
+Then("\"Challenges\" button is visible", () => {
+  cy.contains('Challenges').should('be.visible');
+});
+
+When("Click on \"Start Exploring\" button", () => {
+  cy.contains('Start Exploring').click();
+});
+
+Then("URL includes \"/explore\"", () => {
+  cy.url().should('include','/explore');
+});
+
+When("Click on \"Challenges\" button", () => {
+  cy.contains('Challenges').click();
+});
+
+Then("URL includes \"/challenges\"", () => {
+  cy.url().should('include','/challenges');
+});
+
+Then("Feature card \"Hands-On Practice\" is visible", () => {
+  cy.contains('Hands-On Practice').should('be.visible');
+});
+
+Then("Feature card \"Interview Questions\" is visible", () => {
+  cy.contains('Interview Questions').should('be.visible');
+});
+
+Then("Feature card \"Test Your Skills\" is visible", () => {
+  cy.contains('Test Your Skills').should('be.visible');
+});
+
+Then("Each feature card is clickable", () => {
+  cy.contains('Hands-On Practice').closest('a').should('have.attr','href');
+  cy.contains('Interview Questions').closest('a').should('have.attr','href');
+  cy.contains('Test Your Skills').closest('a').should('have.attr','href');
+});
+
+Then("Banner \"Latest Blogs & Insights\" is visible", () => {
+  cy.contains('Latest Blogs & Insights').should('be.visible');
+});
+
+Then("Article \"HTML Basics\" is visible", () => {
+  cy.contains('HTML Basics').should('be.visible');
+});
+
+Then("Article \"Locator Strategies\" is visible", () => {
+  cy.contains('Locator Strategies').should('be.visible');
+});
+
+Then("Each article has a link and is clickable", () => {
+  cy.contains('HTML Basics').closest('a').should('have.attr','href');
+  cy.contains('Locator Strategies').closest('a').should('have.attr','href');
+});
